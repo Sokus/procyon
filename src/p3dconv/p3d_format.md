3fa3868d9d15fa36c8b8d1793d0fe271fb69c1d8
Sokus <soqs687@gmail.com>
2023-06-04 01:53:31 +0200

    Write .P3D

---
+++ src/p3dconv/p3d_format.md
@@ -0,0 +1,31 @@
+
+## P3D
+```
+[scale]
+[# of vertices]
+  [positions]
+  [normals]
+  [texcoords]
+  [colors]
+[# of indices]
+  [index]
+[# of meshes]
+  [# of indices]
+  [index offset]
+  [has diffuse color?]
+    [diffuse color]
+  [has diffuse texture?]
+    [diffuse texture name]
+```
+
+## PP3D
+```
+[scale]
+[# of meshes]
+  [vertex type]
+  [vertices] (size dependent on vertex type)
+  [indices] (size dependent on vertex type)
+  [has diffuse color?]
+    [diffuse color]
+  TODO: diffuse color map
+```
--- src/p3dconv/win32_main.c
+++ src/p3dconv/win32_main.c
@@ -2,6 +2,10 @@
 
 #define M3D_IMPLEMENTATION
 #include "m3d.h"
+#include "HandmadeMath.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
 #include <stdio.h>
 
@@ -80,30 +84,34 @@ void pe_m3d_free_callback(void *buffer) {
     pe_free(pe_m3d_allocator, buffer);
 }
 
-// *.p3d
-// [# of vertices]
-//   [positions]
-//   [normals]
-//   [texcoords]
-//   [colors]
-// [# of indices]
-//   [index]
-// [# of meshes]
-//   [# of indices]
-//   [index offset]
-//   [has diffuse color?]
-//     [diffuse color]
-//   TODO: diffuse color map
+typedef struct peColor {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+} peColor;
 
-// *.pp3d
-// [# of meshes]
-//   [vertex type]
-//   [vertices] (size dependent on vertex type)
-//   [indices] (size dependent on vertex type)
-//   [has diffuse color?]
-//     [diffuse color]
-//   TODO: diffuse color map
+#define PE_COLOR_WHITE (peColor){ 255, 255, 255, 255 }
 
+HMM_Vec4 pe_color_to_vec4(peColor color) {
+    HMM_Vec4 vec4 = {
+        .R = (float)color.r / 255.0f,
+        .G = (float)color.g / 255.0f,
+        .B = (float)color.b / 255.0f,
+        .A = (float)color.a / 255.0f,
+    };
+    return vec4;
+}
+
+peColor pe_color_uint32(uint32_t color_uint32) {
+    peColor color = {
+        .r = (uint8_t)((color_uint32 >>  0) & 0xFF),
+        .g = (uint8_t)((color_uint32 >>  8) & 0xFF),
+        .b = (uint8_t)((color_uint32 >> 16) & 0xFF),
+        .a = (uint8_t)((color_uint32 >> 24) & 0xFF),
+    };
+    return color;
+}
 
 void pe_p3d_convert(char *source, char *destination) {
     peArena temp_arena;
@@ -113,7 +121,204 @@ void pe_p3d_convert(char *source, char *destination) {
     pe_m3d_current_path = source;
 
     peFileContents m3d_file_contents = pe_file_read_contents(temp_allocator, source, false);
-    m3d_t *m3d = m3d_load(m3d_file_contents.data, pe_m3d_read_callback, pe_m3d_free_callback, NULL);
+    m3d_t *m3d = m3d_load(m3d_file_contents.data, /*pe_m3d_read_callback*/NULL, pe_m3d_free_callback, NULL);
+
+    struct peTempVertexInfo {
+        HMM_Vec3 position;
+        HMM_Vec3 normal;
+        HMM_Vec2 uv;
+        peColor color;
+
+        uint32_t material_id;
+        uint32_t vertex_index_all;
+        uint32_t vertex_index_mesh;
+    };
+    struct peTempVertexInfo *temp_vertex_info = pe_alloc(temp_allocator, m3d->numvertex*sizeof(struct peTempVertexInfo));
+    for (M3D_INDEX i = 0; i < m3d->numvertex; i += 1) {
+        temp_vertex_info[i].material_id = M3D_UNDEF;
+        temp_vertex_info[i].vertex_index_all = UINT32_MAX;
+        temp_vertex_info[i].vertex_index_mesh = UINT32_MAX;
+    }
+
+    struct peTempMeshInfo {
+        unsigned int num_vertex;
+        uint32_t num_index;
+        uint32_t index_offset;
+        bool has_diffuse_color;
+        peColor diffuse_color;
+        bool has_texture_coordinates;
+        bool has_diffuse_texture;
+        char *diffuse_texture_name;
+    };
+    struct peTempMeshInfo *temp_mesh_info = pe_alloc(temp_allocator, (m3d->nummaterial+1)*sizeof(struct peTempMeshInfo));
+    pe_zero_size(temp_mesh_info, (m3d->nummaterial+1)*sizeof(struct peTempMeshInfo));
+
+    uint32_t num_vertex = 0;
+
+    for (M3D_INDEX f = 0; f < m3d->numface; f += 1) {
+        M3D_INDEX mesh_index = (m3d->face[f].materialid == M3D_UNDEF ? m3d->nummaterial : m3d->face[f].materialid);
+        temp_mesh_info[mesh_index].num_index += 3;
+        for (M3D_INDEX m = mesh_index+1; m < m3d->nummaterial+1; m += 1) {
+            temp_mesh_info[m].index_offset += 3;
+        }
+        for (int v = 0; v < 3; v += 1) {
+            M3D_INDEX vert_index = m3d->face[f].vertex[v];
+            temp_vertex_info[vert_index].position.X = m3d->vertex[vert_index].x;
+            temp_vertex_info[vert_index].position.Y = m3d->vertex[vert_index].y;
+            temp_vertex_info[vert_index].position.Z = m3d->vertex[vert_index].z;
+            M3D_INDEX norm_index = m3d->face[f].normal[v];
+            temp_vertex_info[vert_index].normal.X = m3d->vertex[norm_index].x;
+            temp_vertex_info[vert_index].normal.Y = m3d->vertex[norm_index].y;
+            temp_vertex_info[vert_index].normal.Z = m3d->vertex[norm_index].z;
+            if (m3d->face[f].texcoord[0] != M3D_UNDEF) {
+			    temp_mesh_info[mesh_index].has_texture_coordinates = true;
+                M3D_INDEX tex_index = m3d->face[f].texcoord[v];
+                temp_vertex_info[vert_index].uv.X = m3d->tmap[tex_index].u;
+                temp_vertex_info[vert_index].uv.Y = 1.0f - m3d->tmap[tex_index].v;
+            }
+			temp_vertex_info[vert_index].material_id = mesh_index;
+            if (temp_vertex_info[vert_index].vertex_index_all == UINT32_MAX) {
+                temp_vertex_info[vert_index].vertex_index_all = num_vertex;
+                num_vertex += 1;
+            }
+			if (temp_vertex_info[vert_index].vertex_index_mesh == UINT32_MAX) {
+				temp_vertex_info[vert_index].vertex_index_mesh = temp_mesh_info[mesh_index].num_vertex;
+				temp_mesh_info[mesh_index].num_vertex += 1;
+			}
+        }
+    }
+
+    for (M3D_INDEX m = 0; m < m3d->nummaterial; m += 1) {
+        for (M3D_INDEX p = 0; p < m3d->material[m].numprop; p += 1) {
+            switch (m3d->material[m].prop[p].type) {
+                case m3dp_Kd: {
+                    temp_mesh_info[m].has_diffuse_color = true;
+                    temp_mesh_info[m].diffuse_color = pe_color_uint32(m3d->material[m].prop[p].value.color);
+                } break;
+
+                case m3dp_map_Kd: { /* diffuse map */
+                    m3dtx_t *m3d_texture = &m3d->texture[m3d->material[m].prop[p].value.textureid];
+                    temp_mesh_info[m].diffuse_texture_name = m3d_texture->name;
+                } break;
+
+                // UNUSED PROPERTIES
+                case m3dp_Ka: { uint32_t ambient = m3d->material[m].prop[p].value.color; } break;
+                case m3dp_Ks: { uint32_t specular = m3d->material[m].prop[p].value.color; } break;
+                case m3dp_Pr: { float roughness = m3d->material[m].prop[p].value.fnum; } break;
+                case m3dp_Pm: { float metallic = m3d->material[m].prop[p].value.fnum; } break;
+                case m3dp_Ni: { float refraction = m3d->material[m].prop[p].value.fnum; } break;
+
+                // IGNORED PROPERTIES
+                case m3dp_Km:     // [6] bump
+                case m3dp_d:      // [7] dissolve (obsolete)
+                case m3dp_il:     // [8] illumination model
+                case m3dp_map_Km: // [134] bump map
+                case m3dp_map_D:  // [135] disolve map (obsolete)
+                    break;
+
+                default: printf("unknown property: %u\n", m3d->material[m].prop[p].type); break;
+            }
+        }
+    }
+
+    HANDLE dest_file_handle = CreateFileA(destination, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (dest_file_handle == INVALID_HANDLE_VALUE) {
+        printf("could not create file\n");
+        return;
+    }
+
+    // scale
+    WriteFile(dest_file_handle, &m3d->scale, sizeof(m3d->scale), NULL, NULL);
+
+    // # of vertices
+    WriteFile(dest_file_handle, &num_vertex, sizeof(uint32_t), NULL, NULL);
+
+    // positions
+    for (M3D_INDEX v = 0; v < m3d->numvertex; v += 1) {
+        if (temp_vertex_info[v].vertex_index_all != UINT32_MAX) {
+            int16_t write_position_x = (int16_t)(temp_vertex_info[v].position.X * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_position_x, sizeof(int16_t), NULL, NULL);
+            int16_t write_position_y = (int16_t)(temp_vertex_info[v].position.Y * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_position_y, sizeof(int16_t), NULL, NULL);
+            int16_t write_position_z = (int16_t)(temp_vertex_info[v].position.Z * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_position_z, sizeof(int16_t), NULL, NULL);
+        }
+    }
+
+    // normals
+    for (M3D_INDEX v = 0; v < m3d->numvertex; v += 1) {
+        if (temp_vertex_info[v].vertex_index_all != UINT32_MAX) {
+            int16_t write_normal_x = (int16_t)(temp_vertex_info[v].normal.X * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_normal_x, sizeof(int16_t), NULL, NULL);
+            int16_t write_normal_y = (int16_t)(temp_vertex_info[v].normal.Y * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_normal_y, sizeof(int16_t), NULL, NULL);
+            int16_t write_normal_z = (int16_t)(temp_vertex_info[v].normal.Z * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_normal_z, sizeof(int16_t), NULL, NULL);
+        }
+    }
+
+    // texcoords
+    for (M3D_INDEX v = 0; v < m3d->numvertex; v += 1) {
+        if (temp_vertex_info[v].vertex_index_all != UINT32_MAX) {
+            int16_t write_texcoord_x = (int16_t)(temp_vertex_info[v].uv.X * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_texcoord_x, sizeof(int16_t), NULL, NULL);
+            int16_t write_texcoord_y = (int16_t)(temp_vertex_info[v].uv.Y * (float)INT16_MAX);
+            WriteFile(dest_file_handle, &write_texcoord_y, sizeof(int16_t), NULL, NULL);
+        }
+    }
+
+    // colors
+    for (M3D_INDEX v = 0; v < m3d->numvertex; v += 1) {
+        if (temp_vertex_info[v].vertex_index_all != UINT32_MAX) {
+            WriteFile(dest_file_handle, &temp_vertex_info[v].color, sizeof(peColor), NULL, NULL);
+        }
+    }
+
+    // # of indices
+    {
+        uint32_t write_num_index = 3 * m3d->numface;
+        WriteFile(dest_file_handle, &write_num_index, sizeof(uint32_t), NULL, NULL);
+    }
+
+    // index
+    for (M3D_INDEX v = 0; v < m3d->numvertex; v += 1) {
+        if (temp_vertex_info[v].vertex_index_all != UINT32_MAX) {
+            WriteFile(dest_file_handle, &temp_vertex_info[v].vertex_index_all, sizeof(uint32_t), NULL, NULL);
+        }
+    }
+
+    // # of meshes
+    uint16_t write_num_meshes = (uint16_t)(temp_mesh_info[m3d->nummaterial].num_index > 0 ? m3d->nummaterial+1 : m3d->nummaterial);
+    WriteFile(dest_file_handle, &write_num_meshes, sizeof(uint16_t), NULL, NULL);
+
+    for (uint16_t m = 0; m < write_num_meshes; m += 1) {
+        // # of indices
+        WriteFile(dest_file_handle, &temp_mesh_info[m].num_index, sizeof(uint32_t), NULL, NULL);
+
+        // index offset
+        WriteFile(dest_file_handle, &temp_mesh_info[m].index_offset, sizeof(uint32_t), NULL, NULL);
+
+        // has diffuse color?
+        uint8_t write_has_diffuse = temp_mesh_info[m].has_diffuse_color ? 1 : 0;
+        WriteFile(dest_file_handle, &write_has_diffuse, sizeof(uint8_t), NULL, NULL);
+        if (temp_mesh_info[m].has_diffuse_color) {
+            // diffuse color
+            WriteFile(dest_file_handle, &temp_mesh_info[m].diffuse_color, sizeof(peColor), NULL, NULL);
+        }
+
+        // has diffuse texture?
+        uint8_t write_has_diffuse_texture = temp_mesh_info[m].has_diffuse_texture ? 1 : 0;
+        WriteFile(dest_file_handle, &write_has_diffuse_texture, sizeof(uint8_t), NULL, NULL);
+        if (temp_mesh_info[m].has_diffuse_texture) {
+            if (temp_mesh_info[m].diffuse_texture_name != NULL) {
+                size_t diffuse_texture_name_length = strnlen(temp_mesh_info[m].diffuse_texture_name, 512);
+                WriteFile(dest_file_handle, temp_mesh_info[m].diffuse_texture_name, (DWORD)(diffuse_texture_name_length*sizeof(char)), NULL, NULL);
+            }
+        }
+    }
+
+    CloseHandle(dest_file_handle);
+
 
     m3d_free(m3d);
     pe_arena_free(&temp_arena);
