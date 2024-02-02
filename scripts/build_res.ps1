e96d9e69ffe03a8c3c5ca2b794672ce43062ce8a
Sokus <soqs687@gmail.com>
2024-02-02 17:10:40 +0100

    Add build_res.ps1

---
+++ scripts/build_res.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/env powershell
+
+$blender = ""
+if (-not $blender -and $env:PROCYON_BLENDER_PATH) {
+    $blender = ${env:PROCYON_BLENDER_PATH}
+}
+if (-not $blender) {
+    $blender = "blender"
+}
+
+$blender_not_found_message = @"
+build_res.ps1 : Could not start Blender with "${blender}"
+Make sure the command is available or add executable path to PROCYON_BLENDER_PATH
+"@
+if (-not (Test-Path -Path $blender -PathType Leaf)) {
+    Write-Host $blender_not_found_message
+    exit 1
+}
+
+Push-Location "${PSScriptRoot}/.."
+
+& "$blender" ./res/fox.blend -b -P ./src/blender/p3d_export.py -- --output ./res/fox.pp3d --portable --bone_weight_size 4
+
+Pop-Location
