13298e1edc6055162187da14be4abf28a16abba7
Sokus <soqs687@gmail.com>
2024-03-25 23:47:30 +0100

    Add Focus editor

--- scripts/build_psp.ps1
+++ scripts/build_psp.ps1
@@ -2,11 +2,22 @@
 
 $repository_root = $(Resolve-Path "${PSScriptRoot}/..")
 
+$cmake_exit_code = 0
 Push-Location $repository_root
 wsl.exe --distribution Ubuntu export PSPDEV=~/pspdev`; export PATH=`$PATH:`$PSPDEV/bin`; psp-cmake -B psp_build
+$cmake_configure_exit_code = $LASTEXITCODE
 Pop-Location
 
+if ($cmake_exit_code -ne 0) {
+    exit $cmake_exit_code
+}
+
+$make_exit_code = 0
 Push-Location $repository_root/psp_build
 wsl.exe --distribution Ubuntu make
+$make_exit_code = $LASTEXITCODE
 Pop-Location
 
+if ($make_exit_code -ne 0) {
+    exit $make_exit_code
+}
