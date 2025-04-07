6ef3a41c2bea284abb42a51267b6c7f80652879d
Sokus <soqs687@gmail.com>
2025-04-07 12:09:02 +0200

    Change 'pe' prefix to 'p' (Random)

--- src/core/p_random.h
+++ src/core/p_random.h
@@ -3,25 +3,25 @@
 
 #include <stdint.h>
 
-typedef struct peRandom {
+typedef struct pRandom {
 	uint32_t state;
-} peRandom;
+} pRandom;
 
-peRandom pe_random_from_seed(uint32_t seed);
-peRandom pe_random_from_time(void);
-uint32_t pe_random_uint32(peRandom *random);
+pRandom p_random_from_seed(uint32_t seed);
+pRandom p_random_from_time(void);
+uint32_t p_random_uint32(pRandom *random);
 
 #endif // P_RANDOM_HEADER_GUARD
 #if defined(P_CORE_IMPLEMENTATION) && !defined(P_RANDOM_IMPLEMENTATION_GUARD)
 
-peRandom pe_random_from_seed(uint32_t seed) {
-    peRandom result = {
+pRandom p_random_from_seed(uint32_t seed) {
+    pRandom result = {
         .state = seed
     };
     return result;
 }
 
-static uint32_t jenkins_one_at_a_time_hash(const void *data, size_t size) {
+static uint32_t p_jenkins_one_at_a_time_hash(const void *data, size_t size) {
     const uint8_t *bytes = (const uint8_t *)data;
     uint32_t hash = 0;
 
@@ -40,14 +40,14 @@ static uint32_t jenkins_one_at_a_time_hash(const void *data, size_t size) {
 
 // TODO: the user should hash the time and pass it as seed
 #if 0
-peRandom pe_random_from_time(void) {
+pRandom p_random_from_time(void) {
     peTime time = pe_time_local();
-    uint32_t seed = jenkins_one_at_a_time_hash(&time, sizeof(time));
-    return pe_random_from_seed(seed);
+    uint32_t seed = p_jenkins_one_at_a_time_hash(&time, sizeof(time));
+    return p_random_from_seed(seed);
 }
 #endif
 
-uint32_t pe_random_uint32(peRandom *random) {
+uint32_t p_random_uint32(pRandom *random) {
     // https://en.wikipedia.org/w/index.php?title=Linear_congruential_generator&oldid=1169975619
     uint64_t a = 1664525;
     uint64_t c = 1013904223;
