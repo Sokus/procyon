244b33ce8a0d4a28fbcfa9e4362838259a3f0a40
Sokus <soqs687@gmail.com>
2026-09-23 21:52:08 +0200

    p_tests

--- src/samples/p_tests.c
+++ src/samples/p_tests.c
@@ -7,20 +7,15 @@
 #include "platform/p_window.h"
 #include "utility/p_trace.h"
 
-#include <stdio.h>
+#if defined(__PSP__)
+#include <pspdebug.h>
+#endif
 
-#include "HandmadeMath.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
     p_window_init(960, 540, "Procyon");
     p_trace_init();
-
-#if !defined(PSP)
-    #define PE_MODEL_EXTENSION ".p3d"
-#else
-    #define PE_MODEL_EXTENSION ".pp3d"
-#endif
-    pModel model = p_model_load("./res/fox" PE_MODEL_EXTENSION);
 
     // pCamera camera = {
     //     .target = {0.0f, 0.7f, 0.0f},
@@ -38,37 +33,16 @@ int main(int argc, char *argv[]) {
 
     float average_frame_duration = 0.0f;
 
-    while(p_window_should_quit()) {
+    while(!p_window_should_quit()) {
         uint64_t frame_start_time = p_time_now();
         pTraceMark tm_game_loop = P_TRACE_MARK_BEGIN("game loop");
-
-
         // p_camera_update(camera);
 
         p_window_frame_begin();
-        p_clear_background((pColor){ 20, 20, 20, 255 });
-
-        {
-            HMM_Vec2 draw_x_range = HMM_V2(-5.0f, 5.0f);
-            HMM_Vec2 draw_z_range = HMM_V2(-12.0f, 2.0f);
-            int models_to_draw_x = 4;
-            int models_to_draw_z = 4;
-
-            float diff_x = (draw_x_range.Elements[1] - draw_x_range.Elements[0]) / (float)(models_to_draw_x - 1);
-            float diff_z = (draw_z_range.Elements[1] - draw_z_range.Elements[0]) / (float)(models_to_draw_z - 1);
-
-            for (int z = 0; z < models_to_draw_z; z += 1) {
-                for (int x = 0; x < models_to_draw_x; x += 1) {
-                    float position_x = draw_x_range.Elements[0] + ((float)x * diff_x);
-                    float position_z = draw_z_range.Elements[0] + ((float)z * diff_z);
-                    pVec3 position = p_vec3(position_x, 0.0f, position_z);
-                    p_model_draw(&model, position, p_vec3(0.0f, 0.0f, 0.0f));
-                }
-            }
-        }
-
-        // p_model_draw(&model, &temp_arena, HMM_V3(0.0f, 0.0f, 0.0f), HMM_V3(0.0f, 0.0f, 0.0f));
-
+#if defined(__PSP__)
+        pspDebugScreenClear();
+        pspDebugScreenPrintf("bla bla\n");
+#endif
         p_window_frame_end(false);
 
         p_scratch_clear();
